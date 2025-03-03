15e2c8e35b2b517898c1f58ef5b63861a9dd2307
Broky2022 <nhktb2k3@gmail.com>
2025-03-03 10:39:00 +0700

    [update] enhance db.json structure, improve main.js functionality, and update test.html layout

--- src/main.js
+++ src/main.js
@@ -1,37 +1,98 @@
 let URL = "http://localhost:3000/posts";
-
-// fetch(URL).then(function(data) {
-//     return data.json();
-// }).then(function(data) {
-//     console.log(data);
-// });
-
-function load() {
-    fetch(URL).then(function(response) {
+var global;
+LoadSync();
+function Load(){
+    fetch(URL).then(function(response){
         return response.json();
-    }).then(function(data) {
+    }).then(function(data){
         console.log(data);
-    });
+    }).catch(function(error){
+        console.log(error);
+    })
 }
-
-async function loadSync() {
+async function LoadSync(){
     try {
         let response = await fetch(URL);
-        let data = await response.json();
-        for (let i = 0; i < data.length; i++) {
-            console.log(convertFromObjToHTML(data[i]));
+        let posts = await response.json();
+        posts = posts.filter(p=>!p.isDeleted);
+        global = posts;
+        let body = document.getElementById("body");
+        body.innerHTML="";
+        for (const post of posts) {
+            body.innerHTML+=ConvertFromObjToHTML(post);
         }
     } catch (error) {
         console.log(error);
     }
 }
-
-function convertFromObjToHTML(post) {
-    let string = '<st>';
-    string += '<h2>' + post.title + '</h2>';
-    string += '<p>' + post.content + '</p>';
-    string += '</st>';
+function ConvertFromObjToHTML(post){
+    let string = `<tr>`;
+    string +=`<td>${post.id}</td>`
+    string +=`<td>${post.title}</td>`
+    string +=`<td>${post.views}</td>`
+    string +=`<td><button onclick="Delete(${post.id});return false">Delete </button></td>`
+    string += `</tr>`
     return string;
 }
 
-loadSync();
+function CheckExist(id){
+    return global.find(function(p){
+        return p.id==id
+    })
+}
+function getMax(){
+    let ids = global.map(p=>Number.parseInt(p.id)); 
+    return Math.max(...ids);
+}
+
+function Save(){
+    let id = document.getElementById("id").value;
+    if(id.length==0||isNaN(id)){
+        id = (getMax()+1)+"";
+    }
+    let Obj = {
+        id:id,
+        title:document.getElementById("title").value,
+        views:document.getElementById("views").value
+    }
+    if(CheckExist(id)){
+        fetch(URL+"/"+id,{
+            method:'PUT',
+            body:JSON.stringify(Obj),
+            headers:{
+                "Content-Type":"application/json",
+            }
+        }).then(function(){
+            LoadSync();
+        })
+    }else{
+        fetch(URL,{
+            method:'POST',
+            body:JSON.stringify(Obj),
+            headers:{
+                "Content-Type":"application/json",
+            }
+        }).then(function(){
+            LoadSync();
+        })
+    }
+}
+
+function Delete(id){
+    let post = CheckExist(id);
+    if(post){
+        post.isDeleted = true;
+        fetch(URL+"/"+id,{
+            method:'PUT',
+            body:JSON.stringify(post),
+            headers:{
+                "Content-Type":"application/json",
+            }
+        }).then(function(){
+            LoadSync();
+        })
+    }else{
+        //
+    }
+    
+}
