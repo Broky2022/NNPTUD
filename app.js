8f40cdd47493e0b4d5313d32cf04cc8a716168b1
Broky2022 <nhktb2k3@gmail.com>
2025-03-31 09:38:39 +0700

    [ADD] implement authentication routes and user management functions

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/roles', require('./routes/roles'));
+app.use('/auth', require('./routes/auth'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,7 +44,10 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500).send({
+    success:false,
+    message: err.message
+  });
 });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,63 +1,47 @@
 var express = require('express');
+const { token } = require('morgan');
 var router = express.Router();
-let userSchema = require('../schemas/user');
+var userControllers = require('../controllers/users')
+let jwt = require('jsonwebtoken');
+const constants = require('../utils/constants');
 
 /* GET users listing. */
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
   try {
-    const { username, fullName, minLogin, maxLogin } = req.query;
-    let query = { isDeleted: false };
-    if (username) query.username = new RegExp(username, 'i');
-    if (fullName) query.fullName = new RegExp(fullName, 'i');
-    if (minLogin) query.loginCount = { $gte: Number(minLogin) };
-    if (maxLogin) query.loginCount = { ...query.loginCount, $lte: Number(maxLogin) };
-    
-    let users = await userSchema.find(query).populate('role');
-    res.status(200).send({
-      success: true,
-      data: users
-    });
+    if (!req.headers || !req.headers.authorization) {
+      throw new Error("ban chua dang nhap")
+    }
+    if (!req.headers.authorization.startsWith("Bearer")) {
+      throw new Error("ban chua dang nhap")
+    }
+    let token = req.headers.authorization.split(" ")[1];
+    let result = jwt.verify(token, constants.SECRET_KEY);
+    let user_id = result.id;
+    if (result.expireIn > Date.now()) {
+      let users = await userControllers.getAllUsers()
+      res.send({
+        success: true,
+        data: users
+      });
+    } else {
+      throw new Error("token het han")
+    }
   } catch (error) {
-    res.status(500).send({
-      success: false,
-      message: error.message
-    });
+    next(error)
   }
 });
-
-router.get('/:id', async function(req, res, next) {
-  try {
-    let id = req.params.id;
-    let user = await userSchema.findById(id).populate('role');
-    res.status(200).send({
-      success: true,
-      data: user
-    });
-  } catch (error) {
-    res.status(404).send({
-      success: false,
-      message: error.message
-    });
-  }
-});
-
-router.post('/', async function(req, res, next) {
+router.post('/', async function (req, res, next) {
   try {
     let body = req.body;
-    let newUser = new userSchema({
-      username: body.username,
-      password: body.password,
-      email: body.email,
-      fullName: body.fullName || "",
-      avatarUrl: body.avatarUrl || "",
-      status: body.status || false,
-      role: body.role,
-      loginCount: body.loginCount || 0
-    });
-    await newUser.save();
+    let newUser = await userControllers.createAnUser(
+      body.username,
+      body.password,
+      body.email,
+      body.role
+    )
     res.status(200).send({
       success: true,
-      data: newUser
+      message: newUser
     });
   } catch (error) {
     res.status(404).send({
@@ -65,91 +49,30 @@ router.post('/', async function(req, res, next) {
       message: error.message
     });
   }
+
 });
-
-router.put('/:id', async function(req, res, next) {
+router.put('/:id', async function (req, res, next) {
   try {
-    let id = req.params.id;
-    let user = await userSchema.findById(id);
-    if (user) {
-      let body = req.body;
-      if (body.username) user.username = body.username;
-      if (body.password) user.password = body.password;
-      if (body.email) user.email = body.email;
-      if (body.fullName) user.fullName = body.fullName;
-      if (body.avatarUrl) user.avatarUrl = body.avatarUrl;
-      if (body.status !== undefined) user.status = body.status;
-      if (body.role) user.role = body.role;
-      if (body.loginCount !== undefined) user.loginCount = body.loginCount;
-      
-      await user.save();
-      res.status(200).send({
-        success: true,
-        data: user
-      });
-    } else {
-      res.status(404).send({
-        success: false,
-        message: "ID không tồn tại"
-      });
-    }
-  } catch (error) {
-    res.status(404).send({
-      success: false,
-      message: error.message
-    });
-  }
-});
-
-router.delete('/:id', async function(req, res, next) {
-  try {
-    let id = req.params.id;
-    let user = await userSchema.findById(id);
-    if (user) {
-      user.isDeleted = true;
-      await user.save();
-      res.status(200).send({
-        success: true,
-        data: user
-      });
-    } else {
-      res.status(404).send({
-        success: false,
-        message: "ID không tồn tại"
-      });
-    }
-  } catch (error) {
-    res.status(404).send({
-      success: false,
-      message: error.message
-    });
-  }
-});
-
-router.post("/verify", async function (req, res) {
-  try {
-    let { email, username } = req.body;
-    if (!email || !username) throw new Error("Vui lòng cung cấp email và username");
-
-    // Tìm user có email và username trùng khớp, chưa bị xóa mềm
-    let user = await userSchema.findOne({ email, username, isDeleted: false });
-    if (!user) throw new Error("Thông tin không hợp lệ");
-
-    // Cập nhật status thành true
-    user.status = true;
-    await user.save();
-
+    let body = req.body;
+    let updatedUser = await userControllers.updateAnUser(req.params.id, body);
     res.status(200).send({
       success: true,
-      message: "Xác minh thành công, status đã được cập nhật",
-      data: user,
+      message: updatedUser
     });
   } catch (error) {
-    res.status(400).send({
-      success: false,
-      message: error.message,
-    });
+    next(error)
   }
 });
+router.delete('/:id', async function (req, res, next) {
+  try {
+    let deleteUser = await userControllers.deleteAnUser(req.params.id);
+    res.status(200).send({
+      success: true,
+      message: deleteUser
+    });
+  } catch (error) {
+    next(error)
+  }
 
+});
 module.exports = router;
